3a9116604d9b933ebba390ac23e51a6d81a9a13b
MaDycloud-MD <md.shoaib.i.makandar@gmail.com>
2025-08-08 22:52:47 +0530

    Firebase Values

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "--------------------------",
+  apiKey: "AIzaSyDWdKQjkf6Rh_o72k7zJA7DHRIlAf5SP_M",
   authDomain: "smart-city-ea357.firebaseapp.com",
   projectId: "smart-city-ea357",
   storageBucket: "smart-city-ea357.appspot.com",
-  messagingSenderId: "----------",  // Real value
-  appId: "-------------------",  // Real value
-  measurementId: "----------------"  // Real value
+  messagingSenderId: "232623747138",  
+  appId: "1:232623747138:web:ee422244b80ed817a4fddb",  
+  measurementId: "G-1ABCDEF2G"  
 };
 
 // Initialize Firebase
