eb07029f66c4a08ba516ed6777bd46752e83154f
MaDycloud-MD <md.shoaib.i.makandar@gmail.com>
2025-08-14 22:20:39 +0530

    Backend Troubleshooting -- Server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,8 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: true, limit: '20mb' }));
-app.use('/uploads', express.static('uploads'));
+// app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Route Mounting
 app.use('/api/reports', reportRoutes);      
