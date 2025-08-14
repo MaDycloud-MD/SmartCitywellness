ad71f3fbf1e7772d922651898a5f53736f3428a7
MaDycloud-MD <md.shoaib.i.makandar@gmail.com>
2025-08-14 22:49:58 +0530

    Backend Troubleshooting -- Server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 require('./config/db');
 
 const reportRoutes = require('./routes/reportRoute');
@@ -8,9 +9,18 @@ const complaintRoutes = require('./routes/complaintRoute');
 const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
+const baseUrl = process.env.REACT_APP_API_URL2;
 
 // Middleware
 app.use(cors());
+
+app.use(cors({
+  origin: ["http://localhost:3000", `${baseUrl}`],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 // app.use('/uploads', express.static('uploads'));
@@ -30,5 +40,5 @@ app.get('/', (req, res) => {
 const API_URL = process.env.REACT_APP_API_URL;
 const PORT = 5000;
 app.listen(PORT, () => {
-  console.log(`Server running at ${API_URL}`);
+  console.log(`Server running at ${API_URL || `http://localhost:${PORT}`}`);
 });
